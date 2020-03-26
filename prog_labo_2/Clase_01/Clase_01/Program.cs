fab5f635254a5702138fe7c1b74f5f5d02868940
AgusFerro <agusferroig@gmail.com>
2020-03-26 17:47:23 -0300

    Primera Clase

---
+++ prog_labo_2/Clase_01/Clase_01/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase_01
+{
+    class Program
+    {
+        //Ejercicio 1
+        static void Main(string[] args)
+        {
+            string number;
+            bool success = false;
+            int numero;
+            int max=0;
+            int min=0;
+            int promedio=0;
+            int f = 0;
+
+            for (int i = 0; i < 5; i ++)
+            {
+                Console.WriteLine("Ingrese numero");
+                number = Console.ReadLine();
+                success = int.TryParse(number, out numero);
+                if(success == true)
+                {
+                    if(f == 0)
+                    {
+                        max = numero;
+                        min = numero;
+                        f++;
+                    }
+
+                    if(max < numero)
+                    {
+                        max = numero;
+                    }
+
+                    if(min > numero)
+                    {
+                        min = numero;
+                    }
+
+                    promedio = promedio + numero;
+                }
+                
+            }
+
+            promedio = promedio / 5;
+
+            Console.WriteLine("El valor maximo es: {0}", max);
+            Console.WriteLine("El valor minimo es: {0}", min);
+            Console.WriteLine("El promedio es: {0}", promedio);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ prog_labo_2/Clase_01/Ejercicio_2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_2
+{
+    class Program
+    {
+        //Ejercicio 2
+        static void Main(string[] args)
+        {
+            string number;
+            int numero;
+            bool success;
+            double alCuadrado;
+            double alCubo;
+
+            Console.WriteLine("Ingrese un numero");
+
+            number = Console.ReadLine();
+            success = int.TryParse(number, out numero);
+
+            if(success == true) 
+            {
+                alCuadrado = Math.Pow(numero, 2);
+                alCubo = Math.Pow(numero, 3);
+
+                Console.WriteLine("{0} al cuadrado es: {1}", numero, alCuadrado);
+                Console.WriteLine("{0} al cubo es: {1}", numero, alCubo);
+
+                Console.ReadKey();
+            }
+            
+        }
+    }
+}
