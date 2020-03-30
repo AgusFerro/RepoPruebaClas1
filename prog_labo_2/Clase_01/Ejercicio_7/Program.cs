dda73fb0f1d84aca42c5c968ba1f93016ec21f6a
AgusFerro <agusferroig@gmail.com>
2020-03-30 10:29:03 -0300

    Ejercicio 07 incompleto

---
+++ prog_labo_2/Clase_01/Ejercicio_7/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_7
+{
+    class Program
+    {
+        //Ejercicio 7
+        static void Main(string[] args)
+        {
+            int diasTotales = 0;
+
+            Console.WriteLine("Ingresa dia de nacimiento");
+            string diaDesde = Console.ReadLine();
+            int diaDesdeNum = Int32.Parse(diaDesde);
+
+            Console.WriteLine("Ingresa mes de nacimiento");
+            string mesDesde = Console.ReadLine();
+            int mesDesdeNum = Int32.Parse(mesDesde);
+
+            Console.WriteLine("Ingresa año de nacimiento");
+            string anoDesde = Console.ReadLine();
+            int anoDesdeNum = Int32.Parse(anoDesde);
+
+            //*********************************************************
+            string fechaHasta = DateTime.Now.Date.ToString("yyyy/MM/dd");
+            char del = '/';
+            string[] values = fechaHasta.Split(del);
+
+            int anoHasta = Int32.Parse(values[0]);
+            int mesHasta = Int32.Parse(values[1]);
+            int diaHasta = Int32.Parse(values[2]);
+
+            for (int i = anoDesdeNum; i < (anoHasta+1); i++)
+            {
+                if (i % 4 == 0)
+                {
+                    if (i % 100 == 0)
+                    {
+                        if (i % 400 == 0)
+                        {
+                            diasTotales = diasTotales + 366;
+                        }
+                        else
+                        {
+                            diasTotales = diasTotales + 365;
+                        }
+                    }
+                    else
+                    {
+                        diasTotales = diasTotales + 366;
+                    }
+                }
+                else
+                {
+                    diasTotales = diasTotales + 365;
+                }
+            }
+
+            Console.WriteLine(diaHasta);
+            Console.ReadKey();
+        }
+    }
+}
--- prog_labo_2/Clase_02/Clase_02/Program.cs
+++ prog_labo_2/Clase_02/Clase_02/Program.cs
@@ -10,6 +10,83 @@ namespace Clase_02
     {
         static void Main(string[] args)
         {
+
+            //Ejercicio 11
+            string numero;
+            int number;
+            bool success;
+            int contador = 0;
+            int promedio = 0;
+            int min = 0;
+            int max = 0;
+            int flag = 0;
+
+            //for (int i = 0; i < 10; i++)
+            //{
+            //    Console.WriteLine("Ingrese numero: ");
+            //    numero = Console.ReadLine();
+            //    number = Int32.Parse(numero);
+
+            //    success = Validacion.Validar(number, -100, 100);
+
+            //    if (success == true)
+            //    {
+            //        contador++;
+            //        promedio = promedio + number;
+
+            //        if (flag == 0) 
+            //        {
+            //            min = number;
+            //            max = number;
+            //            flag++;
+            //        }
+
+            //        if (min > number)
+            //        {
+            //            min = number;
+            //        }
+
+            //        if (max < number)
+            //        {
+            //            max = number;
+            //        }
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("El numero no se encuentra dentro de los valores min y max");
+            //    }
+            //}
+
+            //promedio = promedio / contador;
+
+            //Console.WriteLine("El promedio es {0}",promedio);
+            //Console.WriteLine("El numero min es {0}", min);
+            //Console.WriteLine("El numero max es {0}", max);
+
+            //Console.ReadKey();
+
+            //******************************************************************************************************
+
+            //Ejercicio 12
+
+            string s;
+            char[] c;
+
+            do
+            {
+                Console.WriteLine("Ingrese numero");
+                numero = Console.ReadLine();
+                number = int.Parse(numero);
+                contador = contador + number;
+                Console.WriteLine("Desea continuar? (S/N)");
+                s = Console.ReadLine();
+                c = s.ToCharArray();
+
+                success = ValidarRespuesta.ValidaS_N(c[0]);
+            } while (success == true);
+
+            Console.WriteLine("La suma de los numeros es {0}", contador);
+            Console.ReadKey();
         }
     }
 }
